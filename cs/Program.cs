e1cc54311ff0713f0cf7a6175ca21a69551d0f52
SAWAN KUMAR PARIDA <139310923+sawankumarparida@users.noreply.github.com>
2025-07-29 21:53:10 +0530

    folder conflicts

--- cs/Program.cs
+++ cs/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // Adds MVC
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
