ec9ed349b82903966295e6fadc5a428f6f703119
SAWAN KUMAR PARIDA <139310923+sawankumarparida@users.noreply.github.com>
2025-08-06 23:18:38 +0530

    controller

---
+++ cs/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ProductController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _context.Products.ToList();
+        return View(products);
+    }
+}
