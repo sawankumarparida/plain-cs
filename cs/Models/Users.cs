a78995a852b1b23da3cddc9d9909c9a9156fcc5a
SAWAN KUMAR PARIDA <139310923+sawankumarparida@users.noreply.github.com>
2025-08-11 23:30:39 +0530

    user model class

---
+++ cs/Models/Users.cs
@@ -0,0 +1,9 @@
+namespace cs.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
