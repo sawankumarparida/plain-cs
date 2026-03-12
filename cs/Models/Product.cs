fe72c32d596c06626055ee326d7a1a86750334af
SAWAN KUMAR PARIDA <139310923+sawankumarparida@users.noreply.github.com>
2026-03-12 13:36:10 +0530

    decimal warning

--- cs/Models/Product.cs
+++ cs/Models/Product.cs
@@ -1,7 +1,11 @@
 namespace cs.Models;
+using System.ComponentModel.DataAnnotations.Schema; // Add this using
+
 public class Product
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18,2)")] // 18 total digits, 2 after the decimal
     public decimal Price { get; set; }
 }
